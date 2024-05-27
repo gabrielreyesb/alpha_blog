f0cf1ab1ad781cd154f2bb1d8492fda2d6cec613
Gabriel Reyes <gabriel@handy.la>
2024-05-27 13:23:41 -0600

    Show article page by id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_235544) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_24_000727) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
