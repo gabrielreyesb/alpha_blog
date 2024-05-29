fb6c98e89ec2b6bad01b3d36e8647fe4a9f68662
Gabriel Reyes <gabriel@handy.la>
2024-05-29 11:30:22 -0600

    Edición de artículos agregada

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,37 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
 
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
-        render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            flash[:notice] = "Article was created successfully."
+            redirect_to @article
+        else
+            respond_to do |format|
+                format.html { render :new, status: :unprocessable_entity }
+                format.turbo_stream { render turbo_stream: turbo_stream.replace(@article, partial: 'articles/form', locals: { article: @article }) }
+            end
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            respond_to do |format|
+                format.html { render :new, status: :unprocessable_entity }
+                format.turbo_stream { render turbo_stream: turbo_stream.replace(@article, partial: 'articles/form', locals: { article: @article }) }
+            end
+        end
     end
 
 end
